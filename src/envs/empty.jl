a0b6e80c50de522831cb66dcd78e9e3486abe93a
Jun Tian <tianjun.cpp@gmail.com>
2020-08-09 16:35:34 +0800

    remove AGENT

--- src/envs/empty.jl
+++ src/envs/empty.jl
@@ -1,16 +1,15 @@
 mutable struct EmptyGridWorld
-    world::GridWorldBase{Tuple{Empty,Wall,Agent}}
+    world::GridWorldBase{Tuple{Empty,Wall}}
     agent_pos::CartesianIndex{2}
     agent_view::BitArray{3}
     agent_direction::CartesianIndex{2}
 end
 
 function EmptyGridWorld(;n=8, agent_start_pos=CartesianIndex(2,2), agent_view_size=7)
-    world = GridWorldBase(n, n, EMPTY, WALL, AGENT)
+    world = GridWorldBase(n, n, EMPTY, WALL)
     world[EMPTY, 2:n-1, 2:n-1] .= true
     world[WALL, [1,n], 1:n] .= true
     world[WALL, 1:n, [1,n]] .= true
-    world[AGENT, agent_start_pos] = true
     agent_view = BitArray{3}(undef, 3, agent_view_size, agent_view_size)
     EmptyGridWorld(world, agent_start_pos, agent_view, RIGHT)
 end
@@ -28,7 +27,8 @@ function (w::EmptyGridWorld)(action::CartesianIndex{2})
         # TODO:
         # update w.agent_view
         # https://github.com/maximecb/gym-minigrid/blob/master/gym_minigrid/minigrid.py#L1165
-        switch!(w.world, AGENT, w.agent_pos, dest)
         w.agent_pos = dest
+        w.agent_direction = action
+        true
     end
 end
