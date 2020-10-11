0b3dd45fcbe7b248688b329fa3e2ed12c8786b90
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-10-11 12:13:10 +0530

    remove actions and directions from objects.jl

--- src/objects.jl
+++ src/objects.jl
@@ -1,50 +1,11 @@
-export COLORS, MOVE_FORWARD, TURN_LEFT, TURN_RIGHT, UP, DOWN, LEFT, RIGHT, LRUD, EMPTY, WALL, GOAL, GEM
-export MoveForward, AbstractObject, Empty, Wall, Goal, Door, Gem, Agent
-export get_color
+export COLORS, EMPTY, WALL, GOAL, GEM
+export AbstractObject, Empty, Wall, Goal, Door, Key, Gem, Agent
+export get_color, get_dir, set_dir!
 
 using Crayons
 using Colors
 
 const COLORS = (:red, :green, :blue, :magenta, :yellow, :white)
-
-#####
-# Actions
-#####
-
-struct MoveForward end
-const MOVE_FORWARD = MoveForward()
-
-struct Up end
-const UP = Up()
-(x::Up)(p::CartesianIndex{2}) = p + CartesianIndex(-1, 0)
-
-struct Down end
-const DOWN = Down()
-(x::Down)(p::CartesianIndex{2}) = p + CartesianIndex(1, 0)
-
-struct Left end
-const LEFT = Left()
-(x::Left)(p::CartesianIndex{2}) = p + CartesianIndex(0, -1)
-
-struct Right end
-const RIGHT = Right()
-(x::Right)(p::CartesianIndex{2}) = p + CartesianIndex(0, 1)
-
-const LRUD = Union{Left, Right, Up, Down}
-
-struct TurnRight end
-const TURN_RIGHT = TurnRight()
-struct TurnLeft end
-const TURN_LEFT = TurnLeft()
-
-(x::TurnRight)(::Left) = UP
-(x::TurnRight)(::Up) = RIGHT
-(x::TurnRight)(::Right) = DOWN
-(x::TurnRight)(::Down) = LEFT
-(x::TurnLeft)(::Left) = DOWN
-(x::TurnLeft)(::Up) = LEFT
-(x::TurnLeft)(::Right) = UP
-(x::TurnLeft)(::Down) = RIGHT
 
 #####
 # Objects
@@ -88,6 +49,7 @@ Base.@kwdef mutable struct Agent <: AbstractObject
     color::Symbol=:red
     dir::LRUD
 end
+
 function Base.convert(::Type{Char}, a::Agent)
     if        a.dir === UP
         '↑'
@@ -99,6 +61,7 @@ function Base.convert(::Type{Char}, a::Agent)
         '→'
     end
 end
+
 get_color(a::Agent) = a.color
 get_dir(a::Agent) = a.dir
 set_dir!(a::Agent, d) = a.dir = d
