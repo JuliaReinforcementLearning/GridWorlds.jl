bf6508d2b80ced280802e7b68291f5f526e4c34d
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-10-31 17:26:58 +0530

    parameterize goal reward

--- src/envs/empty.jl
+++ src/envs/empty.jl
@@ -4,6 +4,7 @@ mutable struct EmptyGridWorld <: AbstractGridWorld
     world::GridWorldBase{Tuple{Empty,Wall,Goal}}
     agent_pos::CartesianIndex{2}
     agent::Agent
+    goal_reward::Float64
     reward::Float64
 end
 
@@ -15,8 +16,9 @@ function EmptyGridWorld(;n=8, agent_start_pos=CartesianIndex(2,2), agent_start_d
     w[WALL, 1:n, [1,n]] .= true
     w[GOAL, n-1, n-1] = true
     w[EMPTY, n-1, n-1] = false
+    goal_reward = 1.0
     reward = 0.0
-    EmptyGridWorld(w, agent_start_pos, Agent(dir=agent_start_dir), reward)
+    EmptyGridWorld(w, agent_start_pos, Agent(dir=agent_start_dir), goal_reward, reward)
 end
 
 function (w::EmptyGridWorld)(::MoveForward)
@@ -26,7 +28,7 @@ function (w::EmptyGridWorld)(::MoveForward)
     if !w.world[WALL, dest]
         w.agent_pos = dest
         if w.world[GOAL, w.agent_pos]
-            w.reward = 1.0
+            w.reward = w.goal_reward
         end
     end
     w
