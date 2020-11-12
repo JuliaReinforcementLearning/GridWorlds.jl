6ff98318f41be74c2a3da4b07d2a395fe15f7408
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-11-12 11:56:07 +0530

    include wrappers.jl

--- src/GridWorlds.jl
+++ src/GridWorlds.jl
@@ -13,6 +13,7 @@ include("grid_world_base.jl")
 include("abstract_grid_world.jl")
 include("envs/envs.jl")
 include("render_in_terminal.jl")
+include("wrappers.jl")
 
 function __init__()
     @require Makie="ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("render_with_Makie.jl")
