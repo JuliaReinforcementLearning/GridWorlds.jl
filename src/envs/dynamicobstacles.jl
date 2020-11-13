2036c7cfa7aa19589ccc226a79f20d36dc66cf55
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-11-13 19:33:32 +0530

    refactor

--- src/envs/dynamicobstacles.jl
+++ src/envs/dynamicobstacles.jl
@@ -31,56 +31,38 @@ function (w::DynamicObstacles)(::MoveForward)
     dir = get_dir(w.agent) 
     dest = dir(w.agent_pos)
     if !w.world[WALL, dest]
-        
-        obstacles_replaced = 0
-        
-        while obstacles_replaced < w.num_obstacle
-            old_pos = w.obs_pos_array[obstacles_replaced+1]
-            next_pos = CartesianIndex(old_pos[1]+rand(w.rng, [-1,0,1]),old_pos[2]+rand(w.rng, [-1,0,1])) 
-            if (!w.world[WALL, next_pos]) && (next_pos != CartesianIndex(w.n-1,w.n-1))
-                flag = 0
-                #flag indicates whether 
-                #1)the new-position of the kth obstacle overlaps the new-positions of [1,k-1] obstacles
-                
-                for i = 0:obstacles_replaced-1
-                    if (w.obs_pos_array[i+1]==next_pos)
-                        
-                        flag = 1
-                        break
-                    end
-                end
-
-                if flag == 0
-                    
-                    w.obs_pos_array[obstacles_replaced+1] = next_pos
-                    w.world[OBSTACLE, old_pos] = false
-                    w.world[EMPTY, old_pos] = true
-                    obstacles_replaced += 1   
-                end
-            end   
-        end
-        #println(w.obs_pos_array)
-        for obs in w.obs_pos_array
-            w.world[OBSTACLE, obs] = true
-            w.world[EMPTY, obs] = false
-        end
-        if w.world[OBSTACLE, dest]
-            #end the game
-            println("You crashed")
-        end
-            
+        update_obstacles!(w)
         w.agent_pos = dest
     end
     w
 end
 
+function valid_obstacle_dest(w::DynamicObstacles, pos::CartesianIndex{2})
+    candidate_pos = [CartesianIndex(pos[1] + i, pos[2] + j) for i in [-1, 0, 1] for j in [-1, 0, 1]]
+    filter(p -> w.world[EMPTY, p] || pos == p || w.agent_pos == p, candidate_pos)
+end
+
+function update_obstacles!(w::DynamicObstacles)
+    for (i, pos) in enumerate(w.obs_pos_array)
+        w.world[EMPTY, pos] = true
+        w.world[OBSTACLE, pos] = false
+
+        new_pos = rand(w.rng, valid_obstacle_dest(w, pos))
+        w.obs_pos_array[i] = new_pos
+
+        w.world[EMPTY, new_pos] = false
+        w.world[OBSTACLE, new_pos] = true
+    end
+end
+
+RLBase.get_terminal(w::DynamicObstacles) = w.world[OBSTACLE, w.agent_pos] || w.world[GOAL, w.agent_pos]
+
 function RLBase.reset!(w::DynamicObstacles; agent_start_pos = CartesianIndex(2, 2), agent_start_dir = RIGHT, goal_pos = CartesianIndex(size(w.world)[end] - 1, size(w.world)[end] - 1))
+
     n = size(w.world)[end]
     w.world[EMPTY, 2:n-1, 2:n-1] .= true
     w.world[GOAL, n-1, n-1] = true
     w.world[EMPTY, n-1, n-1] = false
-
-    obs_pos_array = Array{CartesianIndex{2},1}(undef,w.num_obstacle)
 
     obstacles_placed = 0
     while obstacles_placed < w.num_obstacle
@@ -91,7 +73,7 @@ function RLBase.reset!(w::DynamicObstacles; agent_start_pos = CartesianIndex(2, 
             w.world[OBSTACLE, obs_pos] = true
             w.world[EMPTY, obs_pos] = false
             obstacles_placed = obstacles_placed + 1
-            obs_pos_array[obstacles_placed] = obs_pos
+            w.obs_pos_array[obstacles_placed] = obs_pos
         end
     end
     return w
