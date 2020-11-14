9b966b0c1a79a3f074651b1cba47c8c1c3962e52
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-11-14 14:30:23 +0530

    add reward system

--- src/envs/gotodoor.jl
+++ src/envs/gotodoor.jl
@@ -9,6 +9,7 @@ mutable struct GoToDoor{W<:GridWorldBase, R} <: AbstractGridWorld
     target::Door
     target_reward::Float64
     penalty::Float64
+    reward::Float64
     rng::R
 end
 
@@ -22,8 +23,9 @@ function GoToDoor(;n=8, agent_start_pos=CartesianIndex(2,2), rng=Random.GLOBAL_R
 
     target_reward = 1.0
     penalty = -1.0
+    reward = 0.0
 
-    env = GoToDoor(world, agent_start_pos, Agent(dir=RIGHT), doors[begin], target_reward, penalty, rng)
+    env = GoToDoor(world, agent_start_pos, Agent(dir=RIGHT), doors[begin], target_reward, penalty, reward, rng)
 
     reset!(env)
 
@@ -31,13 +33,28 @@ function GoToDoor(;n=8, agent_start_pos=CartesianIndex(2,2), rng=Random.GLOBAL_R
 end
 
 function (w::GoToDoor)(::MoveForward)
+    w.reward = 0.0
     dir = get_dir(w.agent)
     dest = dir(w.agent_pos)
     if dest ∈ CartesianIndices((size(w.world, 2), size(w.world, 3))) && !w.world[WALL,dest]
         w.agent_pos = dest
+        if w.world[w.target, w.agent_pos]
+            w.reward = w.target_reward
+        elseif any([w.world[x, w.agent_pos] for x in w.world.objects[end-3:end] if x != w.target])
+            w.reward = w.penalty
+        end
     end
     w
 end
+
+function (w::GoToDoor)(action::Union{TurnRight, TurnLeft})
+    w.reward = 0.0
+    agent = get_agent(w)
+    set_dir!(agent, action(get_dir(agent)))
+    w
+end
+
+RLBase.get_reward(w::GoToDoor) = w.reward
 
 function RLBase.reset!(w::GoToDoor)
     n = size(w.world)[end]
@@ -51,6 +68,7 @@ function RLBase.reset!(w::GoToDoor)
     end
 
     w.target = rand(w.rng, doors)
+    w.reward = 0.0
 
     door_pos = [CartesianIndex(rand(w.rng, 2:n-1),1),
                 CartesianIndex(rand(w.rng, 2:n-1),n),
