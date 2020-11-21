aad3c679427d0b04a59961051993637b50fa8165
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-11-21 20:17:38 +0530

    use env to represent environment instance

--- src/render_in_terminal.jl
+++ src/render_in_terminal.jl
@@ -1,22 +1,22 @@
 using Crayons
 
-function Base.show(io::IO, ::MIME"text/markdown", gw::AbstractGridWorld)
-    p, d = get_agent_pos(gw), get_agent_dir(gw)
-    w = convert(GridWorldBase, gw)
+function Base.show(io::IO, ::MIME"text/markdown", env::AbstractGridWorld)
+    p, d = get_agent_pos(env), get_agent_dir(env)
+    w = convert(GridWorldBase, env)
 
     println(io, "World:")
     for i in 1:size(w, 2)
         for j in 1:size(w, 3)
-            if CartesianIndex(i, j) ∈ get_agent_view_inds(gw)
+            if CartesianIndex(i, j) ∈ get_agent_view_inds(env)
                 bg = :dark_gray
             else
                 bg = :black
             end
             if i == p[1] && j == p[2]
-                agent = get_agent(gw)
+                agent = get_agent(env)
                 print(io, Crayon(background=bg, foreground=get_color(agent),reset=true), convert(Char, agent))
             else
-                o = get_object(gw)[findfirst(w.world[:, i, j])]
+                o = get_object(env)[findfirst(w.world[:, i, j])]
                 print(io, Crayon(background=bg, foreground=get_color(o),reset=true),  convert(Char, o))
             end
         end
@@ -25,7 +25,7 @@ function Base.show(io::IO, ::MIME"text/markdown", gw::AbstractGridWorld)
     println(io)
 
     println(io, "Agent's view:")
-    v = get_agent_view(gw)
+    v = get_agent_view(env)
     for i in 1:size(v, 2)
         for j in 1:size(v, 3)
             if i == 1 && j == size(v, 3) ÷ 2 + 1
@@ -35,7 +35,7 @@ function Base.show(io::IO, ::MIME"text/markdown", gw::AbstractGridWorld)
                 if isnothing(x)
                     print(io, '_')
                 else
-                    print(io, get_object(gw)[x])
+                    print(io, get_object(env)[x])
                 end
             end
         end
