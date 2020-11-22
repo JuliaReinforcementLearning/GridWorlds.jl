785c471b5a0c11106422e75f57e3015e3f2a0620
Sriyash <poddarsriyash@gmail.com>
2020-11-22 19:25:59 +0530

    multiroom debugged

--- src/envs/multiroom.jl
+++ src/envs/multiroom.jl
@@ -22,22 +22,22 @@ function placeRooms!( n, numLeft, roomList, minSize, maxSize, entryDoorWall, ent
     if length(roomList) == 0
         top = entryDoorPos
     elseif entryDoorWall == 0 #RIGHT
-        top = CartesianIndex(entryDoorPos[1]-sizeX+1, rand(rng, entryDoorPos[2]-sizeY+2:entryDoorPos[2]))
+        top = CartesianIndex(entryDoorPos[1]-sizeX+1, rand(rng, entryDoorPos[2]-sizeY+2:entryDoorPos[2]-1))
     elseif entryDoorWall == 1 #BOTTOM
-        top = CartesianIndex(rand(rng, entryDoorPos[1]-sizeX+2:entryDoorPos[1]), entryDoorPos[2]-sizeY+1)
+        top = CartesianIndex(rand(rng, entryDoorPos[1]-sizeX+2:entryDoorPos[1]-1), entryDoorPos[2]-sizeY+1)
     elseif entryDoorWall == 2 #LEFT
-        top = CartesianIndex(entryDoorPos[1], rand(rng, entryDoorPos[2]-sizeY+2:entryDoorPos[2]))
+        top = CartesianIndex(entryDoorPos[1], rand(rng, entryDoorPos[2]-sizeY+2:entryDoorPos[2]-1))
     elseif entryDoorWall == 3 #TOP
-        top = CartesianIndex(rand(rng, entryDoorPos[1]-sizeX+2:entryDoorPos[1]), entryDoorPos[2])
+        top = CartesianIndex(rand(rng, entryDoorPos[1]-sizeX+2:entryDoorPos[1]-1), entryDoorPos[2])
     else
         return false
     end
     
-    if !(1 ≤ top[1] ≤ n-sizeX+1 && 1 ≤ top[2] ≤ n-sizeY+1)
+    if !(1 ≤ top[1] ≤ n-sizeX && 1 ≤ top[2] ≤ n-sizeY)
         return false
     end
 
-    for room in roomList
+    for room in roomList[1:length(roomList)-1]
         nonOverlap = top[1]+sizeX < room.top[1] || room.top[1] + room.size[1] ≤ top[1] || top[2]+sizeY < room.top[2] || room.top[2] + room.size[2] ≤ top[2]
         if !nonOverlap
             return false
@@ -46,7 +46,7 @@ function placeRooms!( n, numLeft, roomList, minSize, maxSize, entryDoorWall, ent
     push!(roomList, Room(top, CartesianIndex(sizeX,sizeY), entryDoorPos, CartesianIndex(-1,-1)))
 
     if numLeft == 1
-        return True
+        return true
     end
 
     for i=1:8
@@ -54,18 +54,20 @@ function placeRooms!( n, numLeft, roomList, minSize, maxSize, entryDoorWall, ent
         exitDoorWall = rand(rng, wallSet)
         nextEntryWall = (exitDoorWall + 2) % 4
         if exitDoorWall == 0
-            exitDoorPos = CartesianIndex(top[1]+sizeX-1,top[2]+rand(rng, 1:sizeY-1))
+            exitDoorPos = CartesianIndex(top[1]+sizeX-1,top[2]+rand(rng, 1:sizeY-2))
         elseif exitDoorWall == 1
-            exitDoorPos = CartesianIndex(top[1]+rand(rng, 1:sizeX-1),top[2]+sizeY-1)
+            exitDoorPos = CartesianIndex(top[1]+rand(rng, 1:sizeX-2),top[2]+sizeY-1)
         elseif exitDoorWall == 2
-            exitDoorPos = CartesianIndex(top[1],top[2]+rand(rng, 1:sizeY-1))
+            exitDoorPos = CartesianIndex(top[1],top[2]+rand(rng, 1:sizeY-2))
         elseif exitDoorWall == 3
-            exitDoorPos = CartesianIndex(top[1]+rand(rng, 1:sizeX-1),top[2])
+            exitDoorPos = CartesianIndex(top[1]+rand(rng, 1:sizeX-2),top[2])
         else
             return false
         end
         success = placeRooms!(n, numLeft-1, roomList, minSize,  maxSize, nextEntryWall, exitDoorPos)
-        if success break end
+        if success 
+            break 
+        end
     end
     return true
 end
@@ -78,8 +80,8 @@ function MultiRoom(;n=25, minRooms=2, maxRooms=4, maxSize=10, rng=Random.GLOBAL_
     roomList = []
     while length(roomList) < numRooms
         curRoomList = []
-        entryDoorPos = CartesianIndex(rand(rng, 1:n-1),rand(rng, 1:n-1))
-        placeRooms!(n, numRooms, curRoomList, 4, maxSize, 3, entryDoorPos)
+        entryDoorPos = CartesianIndex(rand(rng, 1:n-2),rand(rng, 1:n-2))
+        placeRooms!(n, numRooms, curRoomList, 4, maxSize, 2, entryDoorPos)
         if length(curRoomList) > length(roomList)
             roomList = curRoomList
         end
@@ -87,17 +89,22 @@ function MultiRoom(;n=25, minRooms=2, maxRooms=4, maxSize=10, rng=Random.GLOBAL_
 
     door_colors = COLORS[randperm(rng, length(COLORS))][1:length(roomList)-1]
     for (i, room) in enumerate(roomList)
-        world[WALL, [room.top[2],room.top[2]+room.size[2]-1], room.top[1]:room.top[1]+room.size[1]-1] .=true
-        world[EMPTY, [room.top[2],room.top[2]+room.size[2]-1], room.top[1]:room.top[1]+room.size[1]-1] .=false
+        world[WALL, room.top[1]:room.top[1]+sizeX-1, [room.top[2],room.top[2]+sizeY-1]] .= true
+        world[EMPTY, room.top[1]:room.top[1]+sizeX-1, [room.top[2],room.top[2]+sizeY-1]] .= false
+        
+        world[WALL, [room.top[1],room.top[1]+sizeX-1], room.top[2]:room.top[2]+sizeY-1] .= true
+        world[EMPTY, [room.top[1],room.top[1]+sizeX-1], room.top[2]:room.top[2]+sizeY-1] .= false
+
         if i>1
             world[Door(door_colors[i-1]), room.entryDoor] = true
             world[WALL, room.entryDoor] = false
+            world[EMPTY, room.entryDoor] = false
             roomList[i-1].exitDoor = room.entryDoor
         end
     end
-
-    agent_start_pos = CartesianIndex(roomList[1].top[2]+roomList[1].size[2]/2, roomList[1].top[1]+roomList[1].size[2]/2)
-    goal = CartesianIndex(roomList[length(roomlist)].top[2]+roomList[length(roomlist)].size[2]/2, roomList[length(roomlist)].top[1]+roomList[length(roomlist)].size[2]/2)
+    froom, lroom = roomList[1], roomList[length(roomlist)]
+    agent_start_pos = froom.top + CartesianIndex(rand(rng, 2:froom.size[1]-2), rand(rng, 2:froom.size[2]-2))
+    goal = lroom.top + CartesianIndex(rand(rng, 2:lroom.size[1]-2), rand(rng, 2:lroom.size[2]-2))
 
     world[EMPTY, goal] = false
     world[GOAL, goal] = true
