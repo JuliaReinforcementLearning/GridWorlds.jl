a19d6ed16a7d10d8bca031ec3769691c940a5be3
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-11-26 19:16:51 +0530

    update filename

--- src/GridWorlds.jl
+++ src/GridWorlds.jl
@@ -12,7 +12,7 @@ include("objects.jl")
 include("grid_world_base.jl")
 include("abstract_grid_world.jl")
 include("envs/envs.jl")
-include("render_in_terminal.jl")
+include("terminal_rendering.jl")
 
 function __init__()
     @require Makie="ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("makie_rendering.jl")
