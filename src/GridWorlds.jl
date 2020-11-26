b95393baedeb0a6d02c74f847aa280d8abb3d820
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-11-26 19:11:53 +0530

    rename file to makie_rendering.jl

--- src/GridWorlds.jl
+++ src/GridWorlds.jl
@@ -15,7 +15,7 @@ include("envs/envs.jl")
 include("render_in_terminal.jl")
 
 function __init__()
-    @require Makie="ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("render_with_Makie.jl")
+    @require Makie="ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("makie_rendering.jl")
 end
 
 end
