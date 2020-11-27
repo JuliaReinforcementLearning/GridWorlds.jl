dff8de19ba7ddc7bc2b6568223d373ddec852d31
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-11-27 19:05:44 +0530

    clean emptygridworld.jl and use getters

--- src/envs/emptygridworld.jl
+++ src/envs/emptygridworld.jl
@@ -8,17 +8,17 @@ mutable struct EmptyGridWorld <: AbstractGridWorld
     reward::Float64
 end
 
-function EmptyGridWorld(;n=8, agent_start_pos=CartesianIndex(2,2), agent_start_dir=RIGHT, goal_pos = CartesianIndex(n-1, n-1))
+function EmptyGridWorld(;n = 8, agent_start_pos = CartesianIndex(2,2), agent_start_dir = RIGHT, goal_pos = CartesianIndex(n-1, n-1))
     objects = (EMPTY, WALL, GOAL)
-    w = GridWorldBase(objects, n, n)
+    world = GridWorldBase(objects, n, n)
 
-    w[WALL, [1,n], 1:n] .= true
-    w[WALL, 1:n, [1,n]] .= true
+    world[WALL, [1,n], 1:n] .= true
+    world[WALL, 1:n, [1,n]] .= true
 
     goal_reward = 1.0
     reward = 0.0
 
-    env = EmptyGridWorld(w, agent_start_pos, Agent(dir=agent_start_dir), goal_reward, reward)
+    env = EmptyGridWorld(world, agent_start_pos, Agent(dir = agent_start_dir), goal_reward, reward)
 
     reset!(env, agent_start_pos = agent_start_pos, agent_start_dir = agent_start_dir, goal_pos = goal_pos)
 
@@ -26,30 +26,32 @@ function EmptyGridWorld(;n=8, agent_start_pos=CartesianIndex(2,2), agent_start_d
 end
 
 function (env::EmptyGridWorld)(::MoveForward)
-    dir = get_dir(env.agent)
-    dest = dir(env.agent_pos)
     env.reward = 0.0
-    if !env.world[WALL, dest]
+    world = get_world(env)
+    agent = get_agent(env)
+    dir = get_dir(agent)
+    dest = dir(get_agent_pos(env))
+    if !world[WALL, dest]
         env.agent_pos = dest
-        if env.world[GOAL, env.agent_pos]
+        if world[GOAL, env.agent_pos]
             env.reward = env.goal_reward
         end
     end
-    env
+    return env
 end
 
-RLBase.get_terminal(env::EmptyGridWorld) = env.world[GOAL, env.agent_pos]
+RLBase.get_terminal(env::EmptyGridWorld) = get_world(env)[GOAL, get_agent_pos(env)]
 
 function RLBase.reset!(env::EmptyGridWorld; agent_start_pos = CartesianIndex(2, 2), agent_start_dir = RIGHT, goal_pos = CartesianIndex(size(env.world)[end] - 1, size(env.world)[end] - 1))
-
     env.reward = 0.0
+    world = get_world(env)
     env.agent_pos = agent_start_pos
     agent = get_agent(env)
     set_dir!(agent, agent_start_dir)
 
-    env.world[EMPTY, :, :] .= .!env.world[WALL, :, :]
-    env.world[GOAL, goal_pos] = true
-    env.world[EMPTY, goal_pos] = false
+    world[EMPTY, :, :] .= .!world[WALL, :, :]
+    world[GOAL, goal_pos] = true
+    world[EMPTY, goal_pos] = false
 
     return env
 end
