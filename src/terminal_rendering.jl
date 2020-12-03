0e723f677ebad9bf74beb91d2ef306b1c4ca436d
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-12-04 00:12:46 +0530

    reuse get_world_with_agent method

--- src/terminal_rendering.jl
+++ src/terminal_rendering.jl
@@ -4,28 +4,23 @@ get_background(env::AbstractGridWorld, pos::CartesianIndex{2}, ::Val{:agent_view
 get_background(env::AbstractGridWorld, pos::CartesianIndex{2}, ::Val{:full_view}) = pos in get_agent_view_inds(env) ? :dark_gray : :black
 
 function print_grid(io::IO, env::AbstractGridWorld, view_type)
-    grid = get_grid(env, Val{view_type}())
-    agent = get_agent(env, Val{view_type}())
-    agent_pos = get_agent_pos(env, Val{view_type}())
-    agent_layer = get_agent_layer(grid, agent_pos)
-    grid = cat(agent_layer, grid, dims = 1)
+    world = get_world_with_agent(env, view_type = view_type)
+    objects = get_objects(world)
 
-    objects = (agent, get_objects(env)...)
-
-    for i in 1:size(grid, 2)
-        for j in 1:size(grid, 3)
+    for i in 1:get_height(world)
+        for j in 1:get_width(world)
             pos = CartesianIndex(i, j)
-            idx = findfirst(grid[:, pos])
+            idx = findfirst(world[:, pos])
             if isnothing(idx)
-                o = nothing
+                object = nothing
                 foreground = :white
-                c = '_'
+                char = '_'
             else
-                o = objects[idx]
-                foreground = get_color(o)
-                c = get_char(o)
+                object = objects[idx]
+                foreground = get_color(object)
+                char = get_char(object)
             end
-            print(io, Crayon(background = get_background(env, pos, Val{view_type}()), foreground = foreground, bold = true, reset = true), c)
+            print(io, Crayon(background = get_background(env, pos, Val{view_type}()), foreground = foreground, bold = true, reset = true), char)
         end
         println(io, Crayon(reset = true))
     end
