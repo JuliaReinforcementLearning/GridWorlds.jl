13b03fd725dadd8c93059a4bf97446f08cf1fa50
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-12-12 22:21:49 +0530

    refactor emptygridworld.jl

--- src/envs/emptygridworld.jl
+++ src/envs/emptygridworld.jl
@@ -6,16 +6,19 @@ mutable struct EmptyGridWorld{R} <: AbstractGridWorld
     reward::Float64
     rng::R
     goal_reward::Float64
+    goal_pos::CartesianIndex
 end
 
 function EmptyGridWorld(; height = 8, width = 8, agent_start_pos = CartesianIndex(2, 2), agent_start_dir = RIGHT, goal_pos = CartesianIndex(height - 1, width - 1), rng = Random.GLOBAL_RNG)
     objects = (EMPTY, WALL, GOAL)
     world = GridWorldBase(objects, height, width)
+    room = Room(CartesianIndex(1, 1), height, width)
+    place_room!(world, room)
     agent = Agent(pos = agent_start_pos, dir = agent_start_dir)
     reward = 0.0
     goal_reward = 1.0
 
-    env = EmptyGridWorld(world, agent, reward, rng, goal_reward)
+    env = EmptyGridWorld(world, agent, reward, rng, goal_reward, goal_pos)
 
     reset!(env, agent_start_pos = agent_start_pos, agent_start_dir = agent_start_dir, goal_pos = goal_pos)
 
@@ -24,13 +27,12 @@ end
 
 function RLBase.reset!(env::EmptyGridWorld; agent_start_pos = CartesianIndex(2, 2), agent_start_dir = RIGHT, goal_pos = CartesianIndex(get_height(env) - 1, get_width(env) - 1))
     world = get_world(env)
-    height = get_height(env)
-    width = get_width(env)
 
-    world[:, :, :] .= false
-    world[WALL, [1, height], :] .= true
-    world[WALL, :, [1, width]] .= true
-    world[EMPTY, :, :] .= .!world[WALL, :, :]
+    old_goal_pos = get_goal_pos(env)
+    world[GOAL, old_goal_pos] = false
+    world[EMPTY, old_goal_pos] = true
+
+    set_goal_pos!(env, goal_pos)
     world[GOAL, goal_pos] = true
     world[EMPTY, goal_pos] = false
 
