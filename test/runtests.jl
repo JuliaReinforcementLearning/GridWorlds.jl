9722848598a8efdecc8aa63da7e5503970e17ee3
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-12-12 15:47:00 +0530

    add and remove tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,58 +3,49 @@ using Test
 using Random
 using ReinforcementLearningBase
 
-ENVS = [EmptyGridWorld, FourRooms, GoToDoor, DoorKey, CollectGems, DynamicObstacles, SequentialRooms]
-ENVS_RLBASE = [EmptyGridWorld, FourRooms, GoToDoor, DoorKey, CollectGems, DynamicObstacles, SequentialRooms]
+ENVS = [EmptyGridWorld, FourRooms, SequentialRooms, GoToDoor, DoorKey, CollectGems, DynamicObstacles]
+
+MAX_STEPS = 3000
+NUM_RESETS = 3
+
+get_terminal_rewards(env::Union{EmptyGridWorld, FourRooms, SequentialRooms, DoorKey}) = (env.goal_reward,)
+get_terminal_rewards(env::DynamicObstacles) = (env.goal_reward, env.obstacle_reward)
+get_terminal_rewards(env::CollectGems) = (env.num_gem_init * env.gem_reward,)
+get_terminal_rewards(env::GoToDoor) = (env.target_reward, env.penalty)
 
 @testset "GridWorlds.jl" begin
     for Env in ENVS
         @testset "$(Env)" begin
             env = Env()
-            @test typeof(get_agent_pos(env)) == CartesianIndex{2}
-            @test typeof(get_agent_dir(env)) <: Direction
-            @test size(get_grid(env), 1) == length(get_objects(env))
+            for _ in 1:NUM_RESETS
+                reset!(env)
+                @test get_reward(env) == 0.0
+                @test get_terminal(env) == false
+                if Env == GoToDoor
+                    @test get_state(env) == (get_agent_view(env), env.target)
+                else
+                    @test get_state(env) == get_agent_view(env)
+                end
 
-            for _=1:1000
-                env = env(rand(get_actions(env)))
-                @test 1 ≤ get_agent_pos(env)[1] ≤ get_height(env)
-                @test 1 ≤ get_agent_pos(env)[2] ≤ get_width(env)
-                @test get_world(env)[WALL, get_agent_pos(env)] == false
-                view = get_agent_view(env)
-                @test typeof(view) <: BitArray{3}
-                @test size(view, 1) == length(get_objects(env))
-            end
-        end
-    end
+                total_reward = 0.0
+                for i in 1:MAX_STEPS
+                    action = rand(get_actions(env))
+                    env(action)
+                    total_reward += get_reward(env)
 
-    for Env in ENVS_RLBASE
-        @testset "$(Env) RLBase API" begin
-            env = Env()
+                    @test 1 ≤ get_agent_pos(env)[1] ≤ get_height(env)
+                    @test 1 ≤ get_agent_pos(env)[2] ≤ get_width(env)
+                    @test get_world(env)[WALL, get_agent_pos(env)] == false
 
-            @test get_reward(env) == 0.0
-            @test get_terminal(env) == false
-            if Env == GoToDoor
-                @test get_state(env) == (get_agent_view(env), env.target)
-            else
-                @test get_state(env) == get_agent_view(env)
-            end
+                    if get_terminal(env)
+                        @test total_reward in get_terminal_rewards(env)
+                        break
+                    end
 
-            π = RandomPolicy(env)
-            total_reward = 0.0
-
-            while !get_terminal(env)
-                action = π(env)
-                env(action)
-                total_reward += get_reward(env)
-            end
-
-            if Env == CollectGems
-                @test total_reward == env.num_gem_init * env.gem_reward
-            elseif Env == EmptyGridWorld || Env == FourRooms || Env == DoorKey || Env == SequentialRooms
-                @test total_reward == env.goal_reward
-            elseif Env == GoToDoor
-                @test (total_reward == env.target_reward || total_reward == env.penalty)
-            elseif Env == DynamicObstacles
-                @test (total_reward == env.obstacle_reward || total_reward == env.goal_reward)
+                    if i == MAX_STEPS
+                        @info "$Env not terminated after MAX_STEPS = $MAX_STEPS"
+                    end
+                end
             end
         end
     end
