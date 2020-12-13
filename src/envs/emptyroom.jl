dc8c6778ef2a90cec1813306a9e65eee45cba628
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-12-13 12:24:33 +0530

    refactor EmptyRoom

--- src/envs/emptyroom.jl
+++ src/envs/emptyroom.jl
@@ -9,32 +9,43 @@ mutable struct EmptyRoom{R} <: AbstractGridWorld
     goal_pos::CartesianIndex
 end
 
-function EmptyRoom(; height = 8, width = 8, agent_start_pos = CartesianIndex(2, 2), agent_start_dir = RIGHT, goal_pos = CartesianIndex(height - 1, width - 1), rng = Random.GLOBAL_RNG)
+function EmptyRoom(; height = 8, width = 8, rng = Random.GLOBAL_RNG)
     objects = (EMPTY, WALL, GOAL)
     world = GridWorldBase(objects, height, width)
     room = Room(CartesianIndex(1, 1), height, width)
     place_room!(world, room)
-    agent = Agent(pos = agent_start_pos, dir = agent_start_dir)
+
+    goal_pos = CartesianIndex(height - 1, width - 1)
+    world[GOAL, goal_pos] = true
+    world[EMPTY, goal_pos] = false
+
+    agent = Agent(pos = CartesianIndex(2, 2), dir = RIGHT)
     reward = 0.0
     goal_reward = 1.0
 
     env = EmptyRoom(world, agent, reward, rng, goal_reward, goal_pos)
 
-    reset!(env, agent_start_pos = agent_start_pos, agent_start_dir = agent_start_dir, goal_pos = goal_pos)
+    reset!(env)
 
     return env
 end
 
-function RLBase.reset!(env::EmptyRoom; agent_start_pos = CartesianIndex(2, 2), agent_start_dir = RIGHT, goal_pos = CartesianIndex(get_height(env) - 1, get_width(env) - 1))
+function RLBase.reset!(env::EmptyRoom)
     world = get_world(env)
+    rng = get_rng(env)
 
     old_goal_pos = get_goal_pos(env)
     world[GOAL, old_goal_pos] = false
     world[EMPTY, old_goal_pos] = true
 
-    set_goal_pos!(env, goal_pos)
-    world[GOAL, goal_pos] = true
-    world[EMPTY, goal_pos] = false
+    new_goal_pos = rand(rng, pos -> !world[WALL, pos], world)
+
+    set_goal_pos!(env, new_goal_pos)
+    world[GOAL, new_goal_pos] = true
+    world[EMPTY, new_goal_pos] = false
+
+    agent_start_pos = rand(rng, pos -> !(world[WALL, pos] || world[GOAL, pos]), world)
+    agent_start_dir = rand(rng, DIRECTIONS)
 
     set_agent_pos!(env, agent_start_pos)
     set_agent_dir!(env, agent_start_dir)
