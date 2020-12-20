f3a6f3d9aea06fc0f7e66927ad706906d579bfed
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-12-20 18:29:38 +0530

    update RLBase methods in DoorKey

--- src/envs/doorkey.jl
+++ src/envs/doorkey.jl
@@ -33,7 +33,7 @@ function DoorKey(; height = 7, width = 7, rng = Random.GLOBAL_RNG)
     return env
 end
 
-RLBase.get_actions(env::DoorKey) = (MOVE_FORWARD, TURN_LEFT, TURN_RIGHT, PICK_UP)
+RLBase.action_space(env::DoorKey) = (MOVE_FORWARD, TURN_LEFT, TURN_RIGHT, PICK_UP)
 
 function (env::DoorKey)(::MoveForward)
     objects = get_objects(env)
