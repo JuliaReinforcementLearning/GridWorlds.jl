32445e61ca1abe2551328163cd534881ad317475
Sid-Bhatia-0 <32610387+Sid-Bhatia-0@users.noreply.github.com>
2020-12-28 23:53:55 +0530

    deprecate env indexing (#113)

--- src/envs/dynamicobstacles.jl
+++ src/envs/dynamicobstacles.jl
@@ -35,19 +35,20 @@ function DynamicObstacles(; height = 8, width = 8, num_obstacles = floor(Int, sq
     return env
 end
 
-iscollision(env::DynamicObstacles) = env[OBSTACLE, get_agent_pos(env)]
+iscollision(env::DynamicObstacles) = get_world(env)[OBSTACLE, get_agent_pos(env)]
 
 function (env::DynamicObstacles)(::MoveForward)
+    world = get_world(env)
     update_obstacles!(env)
 
     dir = get_agent_dir(env)
     dest = dir(get_agent_pos(env))
-    if !env[WALL, dest]
+    if !world[WALL, dest]
         set_agent_pos!(env, dest)
     end
 
     set_reward!(env, 0.0)
-    if env[GOAL, get_agent_pos(env)]
+    if world[GOAL, get_agent_pos(env)]
         set_reward!(env, env.terminal_reward)
     elseif iscollision(env)
         set_reward!(env, env.terminal_penalty)
@@ -57,12 +58,13 @@ function (env::DynamicObstacles)(::MoveForward)
 end
 
 function (env::DynamicObstacles)(action::Union{TurnRight, TurnLeft})
+    world = get_world(env)
     update_obstacles!(env)
 
     set_agent_dir!(env, action(get_agent_dir(env)))
 
     set_reward!(env, 0.0)
-    if env[GOAL, get_agent_pos(env)]
+    if world[GOAL, get_agent_pos(env)]
         set_reward!(env, env.terminal_reward)
     elseif iscollision(env)
         set_reward!(env, env.terminal_penalty)
@@ -72,55 +74,58 @@ function (env::DynamicObstacles)(action::Union{TurnRight, TurnLeft})
 end
 
 function valid_obstacle_dest(env::DynamicObstacles, pos::CartesianIndex{2})
+    world = get_world(env)
     candidate_pos = [CartesianIndex(pos[1] + i, pos[2] + j) for i in [-1, 0, 1] for j in [-1, 0, 1]]
-    filter(p -> (env[EMPTY, p] || p == pos), candidate_pos)
+    filter(p -> (world[EMPTY, p] || p == pos), candidate_pos)
 end
 
 function update_obstacles!(env::DynamicObstacles)
+    world = get_world(env)
     for (i, pos) in enumerate(env.obstacle_pos)
-        env[OBSTACLE, pos] = false
-        env[EMPTY, pos] = true
+        world[OBSTACLE, pos] = false
+        world[EMPTY, pos] = true
 
         new_pos = rand(get_rng(env), valid_obstacle_dest(env, pos))
         env.obstacle_pos[i] = new_pos
 
-        env[OBSTACLE, new_pos] = true
-        env[EMPTY, new_pos] = false
+        world[OBSTACLE, new_pos] = true
+        world[EMPTY, new_pos] = false
     end
     
     return env
 end
 
-RLBase.is_terminated(env::DynamicObstacles) = iscollision(env) || env[GOAL, get_agent_pos(env)]
+RLBase.is_terminated(env::DynamicObstacles) = iscollision(env) || get_world(env)[GOAL, get_agent_pos(env)]
 
 function RLBase.reset!(env::DynamicObstacles)
+    world = get_world(env)
     rng = get_rng(env)
 
     for pos in env.obstacle_pos
-        env[OBSTACLE, pos] = false
-        env[EMPTY, pos] = true
+        world[OBSTACLE, pos] = false
+        world[EMPTY, pos] = true
     end
 
     old_goal_pos = get_goal_pos(env)
-    env[GOAL, old_goal_pos] = false
-    env[EMPTY, old_goal_pos] = true
+    world[GOAL, old_goal_pos] = false
+    world[EMPTY, old_goal_pos] = true
 
-    new_goal_pos = rand(rng, pos -> env[EMPTY, pos], env)
+    new_goal_pos = rand(rng, pos -> world[EMPTY, pos], env)
 
     set_goal_pos!(env, new_goal_pos)
-    env[GOAL, new_goal_pos] = true
-    env[EMPTY, new_goal_pos] = false
+    world[GOAL, new_goal_pos] = true
+    world[EMPTY, new_goal_pos] = false
 
     env.obstacle_pos = CartesianIndex{2}[]
 
     for i in 1:env.num_obstacles
-        pos = rand(rng, pos -> env[EMPTY, pos], env)
-        env[OBSTACLE, pos] = true
-        env[EMPTY, pos] = false
+        pos = rand(rng, pos -> world[EMPTY, pos], env)
+        world[OBSTACLE, pos] = true
+        world[EMPTY, pos] = false
         push!(env.obstacle_pos, pos)
     end
 
-    agent_start_pos = rand(rng, pos -> env[EMPTY, pos], env)
+    agent_start_pos = rand(rng, pos -> world[EMPTY, pos], env)
     agent_start_dir = rand(rng, DIRECTIONS)
 
     set_agent_pos!(env, agent_start_pos)
