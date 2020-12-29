c3be2e411e4a7cb8cdddfc9a306ad794246bb3b7
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-12-29 18:56:50 +0530

    import Makie instead of using Makie

--- src/makie_rendering.jl
+++ src/makie_rendering.jl
@@ -1,16 +1,16 @@
 export play
 
-using .Makie
+import .Makie
 
 get_transform(x::Int) = pos -> CartesianIndex(pos[2], x - pos[1] + 1)
 get_center(pos, tile_size, transform) = (transform(pos).I .- (0.5,0.5)) .* reverse(tile_size)
-get_box(pos, tile_size, transform) = FRect2D((transform(pos).I .- (1,1)) .* reverse(tile_size), reverse(tile_size))
+get_box(pos, tile_size, transform) = Makie.FRect2D((transform(pos).I .- (1,1)) .* reverse(tile_size), reverse(tile_size))
 
 get_markersize(object::AbstractObject, tile_size) = reverse(tile_size)
 get_markersize(object::Empty, tile_size) = reverse(tile_size) ./ 5
 
-function init_screen(env_node::Observable{<:AbstractGridWorld}; resolution = (720, 720))
-    scene = Scene(resolution = resolution, raw = true, camera = campixel!)
+function init_screen(env_node::Makie.Observable{<:AbstractGridWorld}; resolution = (720, 720))
+    scene = Makie.Scene(resolution = resolution, raw = true, camera = Makie.campixel!)
 
     height = get_height(env_node[])
     width = get_width(env_node[])
@@ -19,32 +19,32 @@ function init_screen(env_node::Observable{<:AbstractGridWorld}; resolution = (72
     transform = get_transform(height)
 
     area = scene.px_area
-    tile_size = @lift((widths($area)[2] / height, widths($area)[1] / width))
+    tile_size = Makie.@lift((Makie.widths($area)[2] / height, Makie.widths($area)[1] / width))
 
     # 1. paint background
-    poly!(scene, area)
-    scatter!(scene, @lift(map(x -> get_center(x, $tile_size, transform), filter(pos -> !any(get_world($env_node)[:, pos]), $tile_inds))), color = :white, marker = '~', markersize = @lift((reverse($tile_size) ./ 2)))
+    Makie.poly!(scene, area)
+    Makie.scatter!(scene, Makie.@lift(map(x -> get_center(x, $tile_size, transform), filter(pos -> !any(get_world($env_node)[:, pos]), $tile_inds))), color = :white, marker = '~', markersize = Makie.@lift((reverse($tile_size) ./ 2)))
 
     # 2. paint each kind of object
     for object in get_objects(env_node[])
-        scatter!(scene, @lift(broadcast(x -> get_center(x, $tile_size, transform), findall(get_world($env_node)[object, :, :]))), color = get_color(object), marker = get_char(object), markersize = @lift(get_markersize(object, $tile_size)))
+        Makie.scatter!(scene, Makie.@lift(broadcast(x -> get_center(x, $tile_size, transform), findall(get_world($env_node)[object, :, :]))), color = get_color(object), marker = get_char(object), markersize = Makie.@lift(get_markersize(object, $tile_size)))
     end
 
     # 3. paint agent's view
-    view_boxes = @lift(map(pos -> get_box(pos, $tile_size, transform), filter(pos -> pos in tile_inds, get_agent_view_inds($env_node))))
-    poly!(scene, view_boxes, color = "rgba(255,255,255,0.3)")
+    view_boxes = Makie.@lift(map(pos -> get_box(pos, $tile_size, transform), filter(pos -> pos in tile_inds, get_agent_view_inds($env_node))))
+    Makie.poly!(scene, view_boxes, color = "rgba(255,255,255,0.3)")
 
     # 4. paint agent
-    agent = @lift(get_agent($env_node))
-    agent_center = @lift(get_center(get_agent_pos($env_node), $tile_size, transform))
-    scatter!(scene, agent_center, color = @lift(get_color($agent)), marker = @lift(get_char($agent)), markersize = @lift(get_markersize($agent, $tile_size)))
+    agent = Makie.@lift(get_agent($env_node))
+    agent_center = Makie.@lift(get_center(get_agent_pos($env_node), $tile_size, transform))
+    Makie.scatter!(scene, agent_center, color = Makie.@lift(get_color($agent)), marker = Makie.@lift(get_char($agent)), markersize = Makie.@lift(get_markersize($agent, $tile_size)))
 
-    display(scene)
+    Makie.display(scene)
     scene
 end
 
-function init_screen(env_node::Observable{<:Sokoban}; resolution = (720, 720))
-    scene = Scene(resolution = resolution, raw = true, camera = campixel!)
+function init_screen(env_node::Makie.Observable{<:Sokoban}; resolution = (720, 720))
+    scene = Makie.Scene(resolution = resolution, raw = true, camera = Makie.campixel!)
 
     height = get_height(env_node[])
     width = get_width(env_node[])
@@ -53,18 +53,18 @@ function init_screen(env_node::Observable{<:Sokoban}; resolution = (720, 720))
     transform = get_transform(height)
 
     area = scene.px_area
-    tile_size = @lift((widths($area)[2] / height, widths($area)[1] / width))
+    tile_size = Makie.@lift((Makie.widths($area)[2] / height, Makie.widths($area)[1] / width))
 
     # 1. paint background
-    poly!(scene, area)
-    scatter!(scene, @lift(map(x -> get_center(x, $tile_size, transform), filter(pos -> !any(get_world($env_node)[:, pos]), $tile_inds))), color = :white, marker = '~', markersize = @lift((reverse($tile_size) ./ 2)))
+    Makie.poly!(scene, area)
+    Makie.scatter!(scene, Makie.@lift(map(x -> get_center(x, $tile_size, transform), filter(pos -> !any(get_world($env_node)[:, pos]), $tile_inds))), color = :white, marker = '~', markersize = Makie.@lift((reverse($tile_size) ./ 2)))
 
     # 2. paint each kind of object
     for object in get_objects(env_node[])
-        scatter!(scene, @lift(broadcast(x -> get_center(x, $tile_size, transform), findall(get_world($env_node)[object, :, :]))), color = get_color(object), marker = get_char(object), markersize = @lift(get_markersize(object, $tile_size)))
+        Makie.scatter!(scene, Makie.@lift(broadcast(x -> get_center(x, $tile_size, transform), findall(get_world($env_node)[object, :, :]))), color = get_color(object), marker = get_char(object), markersize = Makie.@lift(get_markersize(object, $tile_size)))
     end
 
-    display(scene)
+    Makie.display(scene)
     scene
 end
 
@@ -82,47 +82,47 @@ function play(env::AbstractGridWorld;file_name=nothing,frame_rate=24)
     r: reset!
     q: Quit
     """)
-    env_node = Node(env)
+    env_node = Makie.Node(env)
     scene = init_screen(env_node)
     is_quit = Ref(false)
 
     if !isnothing(file_name)
-        vs = VideoStream(scene)
-        recordframe!(vs)
+        vs = Makie.VideoStream(scene)
+        Makie.recordframe!(vs)
     end
 
-    on(scene.events.keyboardbuttons) do b
-        if ispressed(b, Keyboard.left)
+    Makie.on(scene.events.keyboardbuttons) do b
+        if Makie.ispressed(b, Makie.Keyboard.left)
             env(TURN_LEFT)
             env_node[] = env
-        elseif ispressed(b, Keyboard.right)
+        elseif Makie.ispressed(b, Makie.Keyboard.right)
             env(TURN_RIGHT)
             env_node[] = env
-        elseif ispressed(b, Keyboard.up)
+        elseif Makie.ispressed(b, Makie.Keyboard.up)
             env(MOVE_FORWARD)
             env_node[] = env
-        elseif ispressed(b, Keyboard.w)
+        elseif Makie.ispressed(b, Makie.Keyboard.w)
             env(MOVE_UP)
             env_node[] = env
-        elseif ispressed(b, Keyboard.s)
+        elseif Makie.ispressed(b, Makie.Keyboard.s)
             env(MOVE_DOWN)
             env_node[] = env
-        elseif ispressed(b, Keyboard.a)
+        elseif Makie.ispressed(b, Makie.Keyboard.a)
             env(MOVE_LEFT)
             env_node[] = env
-        elseif ispressed(b, Keyboard.d)
+        elseif Makie.ispressed(b, Makie.Keyboard.d)
             env(MOVE_RIGHT)
             env_node[] = env
-        elseif ispressed(b, Keyboard.p)
+        elseif Makie.ispressed(b, Makie.Keyboard.p)
             env(PICK_UP)
             env_node[] = env
-        elseif ispressed(b, Keyboard.r)
+        elseif Makie.ispressed(b, Makie.Keyboard.r)
             reset!(env)
             env_node[] = env
-        elseif ispressed(b, Keyboard.q)
+        elseif Makie.ispressed(b, Makie.Keyboard.q)
             is_quit[] = true
         end
-        isnothing(file_name) || recordframe!(vs)
+        isnothing(file_name) || Makie.recordframe!(vs)
     end
 
     try
@@ -131,6 +131,6 @@ function play(env::AbstractGridWorld;file_name=nothing,frame_rate=24)
         end
     catch
     end
-    isnothing(file_name) || save(file_name, vs;framerate=frame_rate)
+    isnothing(file_name) || Makie.save(file_name, vs;framerate=frame_rate)
     Makie.GLMakie.destroy!(Makie.GLMakie.global_gl_screen())
 end
