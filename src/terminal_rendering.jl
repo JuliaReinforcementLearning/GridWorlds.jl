8d4348294d6610e882c071d41791b4a1d003c634
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-01-02 23:06:15 +0530

    rename :full_view, :agent_view to :global, :local

--- src/terminal_rendering.jl
+++ src/terminal_rendering.jl
@@ -1,23 +1,21 @@
-get_grid(env::AbstractGridWorld, ::Val{:full_view}) = get_grid(env)
-get_grid(env::AbstractGridWorld, ::Val{:agent_view}) = get_agent_view(env)
+get_grid(env::AbstractGridWorld, ::Val{:global}) = get_grid(env)
+get_grid(env::AbstractGridWorld, ::Val{:local}) = get_agent_view(env)
 
-get_agent_pos(env::AbstractGridWorld, ::Val{:full_view}) = get_agent_pos(env)
-get_agent_pos(env::AbstractGridWorld, view_type_val::Val{:agent_view}) = get_agent_pos(env, view_type_val, get_navigation_style(env))
-get_agent_pos(env::AbstractGridWorld, view_type_val::Val{:agent_view}, ::DirectedNavigation) = CartesianIndex(1, size(get_grid(env, view_type_val), 3) ÷ 2 + 1)
-function get_agent_pos(env::AbstractGridWorld, view_type_val::Val{:agent_view}, ::UndirectedNavigation)
+get_agent_pos(env::AbstractGridWorld, ::Val{:global}) = get_agent_pos(env)
+get_agent_pos(env::AbstractGridWorld, view_type_val::Val{:local}) = get_agent_pos(env, view_type_val, get_navigation_style(env))
+get_agent_pos(env::AbstractGridWorld, view_type_val::Val{:local}, ::DirectedNavigation) = CartesianIndex(1, get_width(get_grid(env, view_type_val)) ÷ 2 + 1)
+function get_agent_pos(env::AbstractGridWorld, view_type_val::Val{:local}, ::UndirectedNavigation)
     grid = get_grid(env, view_type_val)
     CartesianIndex(get_height(grid) ÷ 2 + 1, get_width(grid) ÷ 2 + 1)
 end
 
-get_agent_dir(env::AbstractGridWorld, view_type_val::Val{:full_view}) = get_agent_dir(env, view_type_val, get_navigation_style(env))
-get_agent_dir(env::AbstractGridWorld, ::Val{:full_view}, ::DirectedNavigation) = get_agent_dir(env)
-get_agent_dir(env::AbstractGridWorld, ::Val{:full_view}, ::UndirectedNavigation) = CENTER
-get_agent_dir(env::AbstractGridWorld, view_type_val::Val{:agent_view}) = get_agent_dir(env, view_type_val, get_navigation_style(env))
-get_agent_dir(env::AbstractGridWorld, ::Val{:agent_view}, ::DirectedNavigation) = DOWN
-get_agent_dir(env::AbstractGridWorld, ::Val{:agent_view}, ::UndirectedNavigation) = CENTER
+get_agent_char(env::AbstractGridWorld, ::Val{:global}) = get_char(get_agent(env))
+get_agent_char(env::AbstractGridWorld, view_type_val::Val{:local}) = get_agent_char(env, view_type_val, get_navigation_style(env))
+get_agent_char(env::AbstractGridWorld, ::Val{:local}, ::DirectedNavigation) = get_char(Agent, DOWN)
+get_agent_char(env::AbstractGridWorld, ::Val{:local}, ::UndirectedNavigation) = get_char(get_agent(env))
 
-get_background(env::AbstractGridWorld, pos::CartesianIndex{2}, ::Val{:full_view}) = pos in get_agent_view_inds(env) ? :dark_gray : :black
-get_background(env::AbstractGridWorld, pos::CartesianIndex{2}, ::Val{:agent_view}) = :dark_gray
+get_background(env::AbstractGridWorld, ::Val{:global}, pos::CartesianIndex{2}) = pos in get_agent_view_inds(env) ? :dark_gray : :black
+get_background(env::AbstractGridWorld, ::Val{:local}, pos::CartesianIndex{2}) = :dark_gray
 
 get_color(::Nothing) = :white
 get_char(::Nothing) = '~'
@@ -31,24 +29,23 @@ function get_first_object(grid::AbstractArray{Bool, 3}, objects, pos::CartesianI
     end
 end
 
-function print_grid(io::IO, env::AbstractGridWorld, view_type)
-    grid = get_grid(env, Val{view_type}())
+function Base.show(io::IO, ::MIME"text/plain", env::AbstractGridWorld, view_type::Symbol)
+    view_type_val = Val{view_type}()
+    grid = get_grid(env, view_type_val)
     objects = get_objects(env)
 
-    agent = get_agent(env)
-    agent_pos = get_agent_pos(env, Val{view_type}())
-    agent_dir = get_agent_dir(env, Val{view_type}())
-    agent_char = get_char(agent, agent_dir)
-    agent_color = get_color(agent)
+    agent_pos = get_agent_pos(env, view_type_val)
+    agent_char = get_agent_char(env, view_type_val)
+    agent_color = get_color(get_agent(env))
 
     for i in 1:get_height(grid)
         for j in 1:get_width(grid)
             pos = CartesianIndex(i, j)
             if pos == agent_pos
-                print(io, Crayons.Crayon(background = get_background(env, pos, Val{view_type}()), foreground = agent_color, bold = true, reset = true), agent_char)
+                print(io, Crayons.Crayon(background = get_background(env, view_type_val, pos), foreground = agent_color, bold = true, reset = true), agent_char)
             else
                 object = get_first_object(grid, objects, pos)
-                print(io, Crayons.Crayon(background = get_background(env, pos, Val{view_type}()), foreground = get_color(object), bold = true, reset = true), get_char(object))
+                print(io, Crayons.Crayon(background = get_background(env, view_type_val, pos), foreground = get_color(object), bold = true, reset = true), get_char(object))
             end
         end
         println(io, Crayons.Crayon(reset = true))
@@ -69,12 +66,13 @@ function Base.show(io::IO, ::MIME"text/plain", world::GridWorldBase)
     end
 end
 
-function Base.show(io::IO, ::MIME"text/plain", env::AbstractGridWorld)
-    println(io, "Full View:")
-    print_grid(io, env, :full_view)
+function Base.show(io::IO, mime::MIME"text/plain", env::AbstractGridWorld)
+    println(io, "Global view:")
+    show(io, mime, env, :global)
     println(io)
-    println(io, "Agent's View:")
-    print_grid(io, env, :agent_view)
-    println(io, "reward: $(RLBase.reward(env))")
-    println(io, "is_terminated: $(RLBase.is_terminated(env))")
+    println(io, "Local view:")
+    show(io, mime, env, :local)
+    println(io)
+    println(io, "RLBase.reward(env): $(RLBase.reward(env))")
+    println(io, "RLBase.is_terminated(env): $(RLBase.is_terminated(env))")
 end
