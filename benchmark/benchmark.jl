c81f4df6aa2b5685abc692743ac91b2c6a0ea916
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-02-18 23:09:34 +0530

    fix formatting

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -14,62 +14,64 @@ information = Dict()
 
 ENVS = [GW.EmptyRoom, GW.GridRooms, GW.SequentialRooms, GW.GoToDoor, GW.DoorKey, GW.CollectGems, GW.DynamicObstacles, GW.Sokoban]
 
+function format_benchmark(str::String)
+    l = split(str, "\n")
+    deleteat!(l, (1, 4, 9))
+    return strip.(l)
+end
+
 function write_benchmarks!(information; file = "benchmark.md")
     io = open(file, "w")
 
-    write(io, "# List of Environments")
-    write(io, "\n")
+    write(io, "# List of Environments\n")
 
     for Env in ENVS
-        write(io, "  1. [$(Symbol(Env))](#$(Symbol(Env)))")
-        write(io, "\n")
+        write(io, "  1. [$(Symbol(Env))](#$(Symbol(Env)))\n")
     end
 
     write(io, "\n")
-    write(io, "# Benchmarks")
-    write(io, "\n")
+    write(io, "# Benchmarks\n\n")
 
     for Env in ENVS
         env_benchmark = information[Symbol(Env)]
 
-        write(io, "# $(String(Symbol(Env)))")
-        write(io, "\n")
+        write(io, "# $(String(Symbol(Env)))\n\n")
 
-        write(io, "#### $(String(Symbol(Env)))()")
-        write(io, "\n")
-        write(io, repr("text/plain", env_benchmark[:instantiation]))
-        write(io, "\n")
+        write(io, "#### $(String(Symbol(Env)))()\n\n")
+        for line in format_benchmark(repr("text/plain", env_benchmark[:instantiation]))
+            write(io, line * "\n\n")
+        end
 
-        write(io, "#### RLBase.reset!(env)")
-        write(io, "\n")
-        write(io, repr("text/plain", env_benchmark[:reset!]))
-        write(io, "\n")
+        write(io, "#### RLBase.reset!(env)\n\n")
+        for line in format_benchmark(repr("text/plain", env_benchmark[:reset!]))
+            write(io, line * "\n\n")
+        end
 
-        write(io, "#### RLBase.state(env)")
-        write(io, "\n")
-        write(io, repr("text/plain", env_benchmark[:state]))
-        write(io, "\n")
+        write(io, "#### RLBase.state(env)\n\n")
+        for line in format_benchmark(repr("text/plain", env_benchmark[:state]))
+            write(io, line * "\n\n")
+        end
 
-        write(io, "#### RLBase.action_space(env)")
-        write(io, "\n")
-        write(io, repr("text/plain", env_benchmark[:action_space]))
-        write(io, "\n")
+        write(io, "#### RLBase.action_space(env)\n\n")
+        for line in format_benchmark(repr("text/plain", env_benchmark[:action_space]))
+            write(io, line * "\n\n")
+        end
 
-        write(io, "#### RLBase.is_terminated(env)")
-        write(io, "\n")
-        write(io, repr("text/plain", env_benchmark[:is_terminated]))
-        write(io, "\n")
+        write(io, "#### RLBase.is_terminated(env)\n\n")
+        for line in format_benchmark(repr("text/plain", env_benchmark[:is_terminated]))
+            write(io, line * "\n\n")
+        end
 
-        write(io, "#### RLBase.reward(env)")
-        write(io, "\n")
-        write(io, repr("text/plain", env_benchmark[:reward]))
-        write(io, "\n")
+        write(io, "#### RLBase.reward(env)\n\n")
+        for line in format_benchmark(repr("text/plain", env_benchmark[:reward]))
+            write(io, line * "\n\n")
+        end
 
         for action in keys(env_benchmark[:action_info])
-            write(io, "#### env($action)")
-            write(io, "\n")
-            write(io, repr("text/plain", env_benchmark[:action_info][action]))
-            write(io, "\n")
+            write(io, "#### env($action)\n\n")
+            for line in format_benchmark(repr("text/plain", env_benchmark[:action_info][action]))
+                write(io, line * "\n\n")
+            end
         end
 
     end
@@ -101,3 +103,4 @@ for Env in ENVS
 end
 
 write_benchmarks!(information)
+
