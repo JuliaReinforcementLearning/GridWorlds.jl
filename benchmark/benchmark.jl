b3337d3caae3bb013266a90e0e7885b4397809ca
Sid-Bhatia-0 <32610387+Sid-Bhatia-0@users.noreply.github.com>
2021-03-14 16:38:34 +0530

    fix benchmarking (#131)
    * use relative path for GridWorlds.jl, no need of [compat] entry
    
    * use Refs while benchmarking
    
    * return CartesianIndices for get_interior(::Room)
    
    * add profiling packages and IJulia
    
    * add new envs and update benchmark.md

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -12,7 +12,7 @@ const SEED = 0
 rng = Random.MersenneTwister(SEED)
 information = Dict()
 
-ENVS = [GW.EmptyRoom, GW.GridRooms, GW.SequentialRooms, GW.GoToDoor, GW.DoorKey, GW.CollectGems, GW.DynamicObstacles, GW.Sokoban]
+ENVS = [GW.EmptyRoom, GW.GridRooms, GW.SequentialRooms, GW.Maze, GW.GoToDoor, GW.DoorKey, GW.CollectGems, GW.DynamicObstacles, GW.Sokoban, GW.Snake, GW.Catcher, GW.Transport]
 
 function format_benchmark(str::String)
     l = split(str, "\n")
@@ -26,7 +26,7 @@ function write_benchmarks!(information; file = "benchmark.md")
     write(io, "# List of Environments\n")
 
     for Env in ENVS
-        write(io, "  1. [$(Symbol(Env))](#$(Symbol(Env)))\n")
+        write(io, "  1. $(Symbol(Env))\n")
     end
 
     write(io, "\n")
@@ -83,19 +83,19 @@ for Env in ENVS
 
     env_benchmark = Dict()
 
-    env_benchmark[:instantiation] = BT.@benchmark $Env(rng = $rng)
+    env_benchmark[:instantiation] = BT.@benchmark $(Ref(Env))[](rng = $(Ref(rng))[])
 
     env = Env(rng = rng)
 
-    env_benchmark[:reset!] = BT.@benchmark RLBase.reset!($env)
-    env_benchmark[:state] = BT.@benchmark RLBase.state($env)
-    env_benchmark[:action_space] = BT.@benchmark RLBase.action_space($env)
-    env_benchmark[:is_terminated] = BT.@benchmark RLBase.is_terminated($env)
-    env_benchmark[:reward] = BT.@benchmark RLBase.reward($env)
+    env_benchmark[:reset!] = BT.@benchmark RLBase.reset!($(Ref(env))[])
+    env_benchmark[:state] = BT.@benchmark RLBase.state($(Ref(env))[])
+    env_benchmark[:action_space] = BT.@benchmark RLBase.action_space($(Ref(env))[])
+    env_benchmark[:is_terminated] = BT.@benchmark RLBase.is_terminated($(Ref(env))[])
+    env_benchmark[:reward] = BT.@benchmark RLBase.reward($(Ref(env))[])
 
     action_info = Dict()
     for action in RLBase.action_space(env)
-        action_info[Symbol(action)] = BT.@benchmark $env($action)
+        action_info[Symbol(action)] = BT.@benchmark $(Ref(env))[]($(Ref(action))[])
     end
     env_benchmark[:action_info] = action_info
 
@@ -103,4 +103,3 @@ for Env in ENVS
 end
 
 write_benchmarks!(information)
-
