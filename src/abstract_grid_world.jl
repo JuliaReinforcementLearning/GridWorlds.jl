a5280e6b7b26359ea66cb6adcc880816c91428e5
Siddharth Bhatia <sidb60200@gmail.com>
2021-06-30 18:53:41 +0530

    create new abstract type AbstractGridWorldGame

--- src/abstract_grid_world.jl
+++ src/abstract_grid_world.jl
@@ -5,42 +5,46 @@ get_half_size(env::AbstractGridWorld) = (2 * (get_height(env) ÷ 4) + 1, 2 * (ge
 
 Random.rand(rng::Random.AbstractRNG, f::Function, env::AbstractGridWorld; max_try = 1000) = rand(rng, f, get_world(env), max_try = max_try)
 
-reset!(env::AbstractGridWorld) = error("Method not implemented for $(typeof(env))")
-act!(env::AbstractGridWorld) = error("Method not implemented for $(typeof(env))")
-get_characters(env::AbstractGridWorld) = error("Method not implemented for $(typeof(env))")
-get_action_keys(env::AbstractGridWorld) = error("Method not implemented for $(typeof(env))")
-get_action_names(env::AbstractGridWorld) = error("Method not implemented for $(typeof(env))")
+#####
+##### AbstractGridWorldGame
+#####
 
-function get_tile_map_string(tile_map, characters)
-    num_objects, height, width = size(tile_map)
+abstract type AbstractGridWorldGame end
+
+#####
+##### Game logic methods
+#####
+
+reset!(env::AbstractGridWorldGame) = error("Method not implemented for $(typeof(env))")
+act!(env::AbstractGridWorldGame) = error("Method not implemented for $(typeof(env))")
+
+#####
+##### Optional methods for pretty printing, playing, etc...
+#####
+
+get_tile_pretty_repr(env::AbstractGridWorldGame, i::Integer, j::Integer) = error("Method not implemented for $(typeof(env))")
+get_action_keys(env::AbstractGridWorldGame) = error("Method not implemented for $(typeof(env))")
+get_action_names(env::AbstractGridWorldGame) = error("Method not implemented for $(typeof(env))")
+get_tile_map_height(env::AbstractGridWorldGame) = error("Method not implemented for $(typeof(env))")
+get_tile_map_width(env::AbstractGridWorldGame) = error("Method not implemented for $(typeof(env))")
+
+function get_tile_map_pretty_repr(env::AbstractGridWorldGame)
+    height_tile_map = get_tile_map_height(env)
+    width_tile_map = get_tile_map_width(env)
 
     str = ""
 
-    for i in 1:height
-        for j in 1:width
-            object = findfirst(@view tile_map[:, i, j])
-            if isnothing(object)
-                str = str * characters[end]
-            else
-                str = str * characters[object]
-            end
+    for i in 1:height_tile_map
+        for j in 1:width_tile_map
+            str = str * get_tile_pretty_repr(env, i, j)
         end
-
-        str = str * "\n"
+        if i < height_tile_map
+            str = str * "\n"
+        end
     end
 
     return str
 end
-
-function get_show_string(env::AbstractGridWorld)
-    tile_map = env.tile_map
-    characters = get_characters(env)
-    str = get_tile_map_string(tile_map, characters)
-    str = str * "reward = $(env.reward)\ndone = $(env.done)"
-    return str
-end
-
-Base.show(io::IO, ::MIME"text/plain", env::AbstractGridWorld) = print(io, get_show_string(env))
 
 #####
 # utils
