4e9f8e803343bea414dbec8e1bb056c95176e496
Siddharth Bhatia <sidb60200@gmail.com>
2021-07-20 00:17:15 +0530

    remove macros generate_getters & generate_setters

--- src/abstract_grid_world.jl
+++ src/abstract_grid_world.jl
@@ -34,29 +34,3 @@ function get_tile_map_pretty_repr(env::AbstractGridWorldGame)
 
     return str
 end
-
-#####
-# utils
-#####
-
-macro generate_getters(type)
-    T = getfield(__module__, type)::Union{Type,DataType}
-    defs = Expr(:block)
-    for field in fieldnames(T)
-        get = Symbol(:get_, field)
-        qn = QuoteNode(field)
-        push!(defs.args, :($(esc(get))(instance::$type) = getfield(instance, $qn)))
-    end
-    return defs
-end
-
-macro generate_setters(type)
-    T = getfield(__module__, type)::Union{Type,DataType}
-    defs = Expr(:block)
-    for field in fieldnames(T)
-        set = Symbol(:set_, field, :!)
-        qn = QuoteNode(field)
-        push!(defs.args, :($(esc(set))(instance::$type, x) = setfield!(instance, $qn, x)))
-    end
-    return defs
-end
