c727f65f0b97017bd87c18a43c0c92e17362c7bf
Siddharth Bhatia <sidb60200@gmail.com>
2021-07-21 20:42:32 +0530

    rename actions.jl to navigation.jl

--- src/GridWorlds.jl
+++ src/GridWorlds.jl
@@ -5,7 +5,7 @@ import Random
 import REPL
 import ReinforcementLearningBase as RLBase
 
-include("actions.jl")
+include("navigation.jl")
 include("abstract_grid_world.jl")
 include("play.jl")
 include("envs/envs.jl")
