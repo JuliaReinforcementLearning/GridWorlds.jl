c8781b4017328a75b631ca2026f79110a660e7e7
Siddharth Bhatia <sidb60200@gmail.com>
2021-07-21 16:49:14 +0530

    remove grid_world_base.jl & old directions

--- src/directions.jl
+++ src/directions.jl
@@ -1,23 +1,3 @@
-abstract type AbstractDirection end
-
-struct Up <: AbstractDirection end
-const UP = Up()
-move(::Up, pos::CartesianIndex{2}) = pos + CartesianIndex(-1, 0)
-
-struct Down <: AbstractDirection end
-const DOWN = Down()
-move(::Down, pos::CartesianIndex{2}) = pos + CartesianIndex(1, 0)
-
-struct Left <: AbstractDirection end
-const LEFT = Left()
-move(::Left, pos::CartesianIndex{2}) = pos + CartesianIndex(0, -1)
-
-struct Right <: AbstractDirection end
-const RIGHT = Right()
-move(::Right, pos::CartesianIndex{2}) = pos + CartesianIndex(0, 1)
-
-const DIRECTIONS = (UP, DOWN, LEFT, RIGHT)
-
 const NUM_DIRECTIONS = 4
 const RIGHT_DIRECTION = 0
 const UP_DIRECTION = 1
