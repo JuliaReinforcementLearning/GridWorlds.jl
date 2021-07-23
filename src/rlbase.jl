b28cd9146e7a7ea1dd1302d733f80d9f32e8f06f
Siddharth Bhatia <sidb60200@gmail.com>
2021-07-23 13:27:08 +0530

    don't forward get_action_names for RLBaseEnv

--- src/rlbase.jl
+++ src/rlbase.jl
@@ -3,6 +3,6 @@ struct RLBaseEnv{E} <: RLBase.AbstractEnv
 end
 
 Base.show(io::IO, mime::MIME"text/plain", env::RLBaseEnv{E}) where {E <: AbstractGridWorld} = show(io, mime, env.env)
+
 play!(terminal::REPL.Terminals.UnixTerminal, env::RLBaseEnv{E}; file_name::Union{Nothing, AbstractString} = nothing) where {E <: AbstractGridWorld} = play!(terminal, env.env, file_name = file_name)
 play!(env::RLBaseEnv{E}; file_name = nothing) where {E <: AbstractGridWorld} = play!(REPL.TerminalMenus.terminal, env.env, file_name = file_name)
-get_action_names(env::RLBaseEnv{E}) where {E <: AbstractGridWorld} = get_action_names(env.env)
