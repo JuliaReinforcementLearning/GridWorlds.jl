b1572481e45d03116d05ecdbe384f95cfca7d1e5
Siddharth Bhatia <sidb60200@gmail.com>
2021-07-24 01:53:12 +0530

    remove old implementation of move methods

--- src/navigation.jl
+++ src/navigation.jl
@@ -35,37 +35,3 @@ function move_backward(position::CartesianIndex{2}, direction::Integer)
         return move_left(position)
     end
 end
-
-move_up(i::Integer, j::Integer) = (i - 1, j)
-move_down(i::Integer, j::Integer) = (i + 1, j)
-move_left(i::Integer, j::Integer) = (i, j - 1)
-move_right(i::Integer, j::Integer) = (i, j + 1)
-no_move(i::Integer, j::Integer) = (i, j)
-
-function move_forward(dir::Integer, i::Integer, j::Integer)
-    if dir == UP
-        return move_up(i, j)
-    elseif dir == DOWN
-        return move_down(i, j)
-    elseif dir == LEFT
-        return move_left(i, j)
-    elseif dir == RIGHT
-        return move_right(i, j)
-    else
-        return no_move(i, j)
-    end
-end
-
-function move_backward(dir::Integer, i::Integer, j::Integer)
-    if dir == UP
-        return move_down(i, j)
-    elseif dir == DOWN
-        return move_up(i, j)
-    elseif dir == LEFT
-        return move_right(i, j)
-    elseif dir == RIGHT
-        return move_left(i, j)
-    else
-        return no_move(i, j)
-    end
-end
